91f4ae680f0c3367954740ca74d34e32fb631766
JackTracerSpore <jack.hawkins@mycelium.ventures>
2023-07-27 21:45:44 +0100

    thread

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -3,9 +3,7 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc,Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -14,21 +12,38 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
+
     for _ in 0..10 {
-        let status_shared = Arc::clone(&status);
+        let status_shared: Arc<Mutex<JobStatus>> = Arc::clone(&status);
+
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut num = match status_shared.lock() {
+                Ok(guard) => guard,
+                Err(e) => {
+                    eprintln!("Error acquiring lock: {:?}", e);
+                    return;
+                }
+            };
+
+            num.jobs_completed += 1;
         });
         handles.push(handle);
     }
+
     for handle in handles {
         handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
-        // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", ???);
     }
+
+    // After all threads have finished, print the final value of 'jobs_completed'
+    let final_num = match status.lock() {
+        Ok(guard) => guard,
+        Err(e) => {
+            eprintln!("Error acquiring lock: {:?}", e);
+            return;
+        }
+    };
+    println!("jobs completed {}", final_num.jobs_completed);
 }
