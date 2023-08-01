a60d47dda7be0706d56b43285b3e3cf5524e14d4
JackTracerSpore <jack.hawkins@mycelium.ventures>
2023-08-01 21:39:48 +0100

    shared ownership

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -16,28 +16,25 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
 fn main() {
-    println!("This is an empty cons list: {:?}", create_empty_list());
-    println!(
-        "This is a non-empty cons list: {:?}",
-        create_non_empty_list()
-    );
+    println!("This is an empty cons list: {:?}", List::create_empty_list());
+    println!("This is a non-empty cons list: {:?}", List::create_non_empty_list());
 }
 
-pub fn create_empty_list() -> List {
-    todo!()
-}
+impl List {
+    pub fn create_empty_list() -> List {
+        List::Nil
+    }
 
-pub fn create_non_empty_list() -> List {
-    todo!()
+    pub fn create_non_empty_list() -> List {
+        List::Cons(1, Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))))
+    }
 }
 
 #[cfg(test)]
@@ -46,11 +43,11 @@ mod tests {
 
     #[test]
     fn test_create_empty_list() {
-        assert_eq!(List::Nil, create_empty_list())
+        assert_eq!(List::Nil, List::create_empty_list())
     }
 
     #[test]
     fn test_create_non_empty_list() {
-        assert_ne!(create_empty_list(), create_non_empty_list())
+        assert_ne!(List::create_empty_list(), List::create_non_empty_list())
     }
 }
